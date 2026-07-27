580dbd67e69b7df24d1af15431ecf4a04f0a83fd
malik3251 <malikabass28@gmail.com>
2026-07-27 21:02:41 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,14 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('vault-store-v1').then((cache) => {
-      return cache.addAll(['./index.html']);
+    caches.open('vault-cache-v2').then((cache) => {
+      return cache.addAll(['./index.html', './manifest.json']);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (e) => {
